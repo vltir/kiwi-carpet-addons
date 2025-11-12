b548179e69b37c823dff7105745ee18453c750e3
vltir <87820976+vltir@users.noreply.github.com>
2025-11-12 23:03:50 +0100

    Project Init

---
+++ src/main/java/com/vltir/ExampleCommand.java
@@ -0,0 +1,9 @@
+package com.vltir;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.server.command.ServerCommandSource;
+
+public class ExampleCommand {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+    }
+}
