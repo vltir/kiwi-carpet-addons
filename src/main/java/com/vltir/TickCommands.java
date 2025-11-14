d5c99c4651b27f513cf23123756fd394acf0b892
vltir <87820976+vltir@users.noreply.github.com>
2025-11-14 16:27:05 +0100

    Tick Sprint Easy Interrupt

--- src/main/java/com/vltir/TickCommands.java
+++ src/main/java/com/vltir/TickCommands.java
@@ -1,6 +1,7 @@
 package com.vltir;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.CommandNode;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.minecraft.server.command.ServerCommandSource;
@@ -15,42 +16,30 @@ public class TickCommands {
     }
 
     public static void registerTickSprintEasyInterrupt(CommandDispatcher<ServerCommandSource> dispatcher) {
-        CommandNode<ServerCommandSource> tickNode = dispatcher.getRoot().getChild("tick");
-        if (tickNode == null) {
-            LOGGER.warn("tick command not found");
-            return;
-        }
-        CommandNode<ServerCommandSource> sprintNode = tickNode.getChild("sprint");
-        if (sprintNode == null) {
-            LOGGER.warn("tick sprint command not found");
-            return;
-        }
-        CommandNode<ServerCommandSource> stopNode = sprintNode.getChild("stop");
-        if (stopNode == null) {
+        CommandNode<ServerCommandSource> tickNode, sprintNode, stopNode;
+        if ((tickNode = dispatcher.getRoot().getChild("tick")) == null ||
+                (sprintNode = tickNode.getChild("sprint")) == null ||
+                (stopNode = sprintNode.getChild("stop")) == null) {
             LOGGER.warn("tick sprint stop command not found");
             return;
         }
-        sprintNode.addChild(literal("")
-                .requires(source -> KiwiSettings.tickSprintEasyInterrupt)
-                .executes(stopNode.getCommand())
-                .build()
-        );
+
         LiteralCommandNode<ServerCommandSource> newSprintNode = literal("sprint")
-                .requires(source -> KiwiSettings.tickSprintEasyInterrupt)
-                .executes(stopNode.getCommand())
+                .executes(ctx -> {
+                    if (!KiwiSettings.tickSprintEasyInterrupt) {
+                        throw CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherUnknownCommand().create();
+                    }
+                    return stopNode.getCommand().run(ctx);
+                })
                 .build();
         sprintNode.getChildren().forEach(newSprintNode::addChild);
         tickNode.addChild(newSprintNode);
     }
 
     public static void registerTickWarpAlias(CommandDispatcher<ServerCommandSource> dispatcher) {
-        CommandNode<ServerCommandSource> tickNode = dispatcher.getRoot().getChild("tick");
-        if (tickNode == null) {
-            LOGGER.warn("tick command not found");
-            return;
-        }
-        CommandNode<ServerCommandSource> sprintNode = tickNode.getChild("sprint");
-        if (sprintNode == null) {
+        CommandNode<ServerCommandSource> tickNode, sprintNode;
+        if ((tickNode = dispatcher.getRoot().getChild("tick")) == null ||
+                (sprintNode = tickNode.getChild("sprint")) == null) {
             LOGGER.warn("tick sprint command not found");
             return;
         }
